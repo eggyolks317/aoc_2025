5f597426bd8544467fab84ab3f21141631be29d8
eggyolks317 <yokshi25@kas.kh.edu.tw>
2025-12-03 23:28:14 -0800

    created function to count how many adjacent @

--- src/day4.js
+++ src/day4.js
@@ -12,13 +12,29 @@ export function day4() {
   });
 }
 
-let testCase = ``;
+let testCase = `..@@.@@@@. @@@.@.@.@@ @@@@@.@.@@ @.@@@@..@. @@.@@@@.@@ .@@@@@@@.@ .@.@.@.@@@ @.@@@.@@@@ .@@@@@@@@. @.@.@@@.@.`;
 
 function puzzle1() {
   let input = document.getElementById("input4").value;
+  input = testCase;
   if (input.length == 0) {
     return 0;
   }
+  let lines = input.split(" ");
+  lines = lines.map((line) => line.split(""));
+  for (let i = 0; i < lines.length; i++) {
+    let line = lines[i];
+    for (let j = 0; j < line.length; j++) {
+      let count = 0;
+      if (line[j] == "@") {
+        count = checkAdj(lines, i, j);
+      }
+      if (count < 4) {
+        line[j] = "x";
+      }
+    }
+  }
+  console.log(lines);
 }
 
 function puzzle2() {
@@ -26,4 +42,28 @@ function puzzle2() {
   if (input.length == 0) {
     return 0;
   }
+}
+
+function checkAdj(arr, pos1, pos2) {
+  let count = 0;
+  for (let i = -1; i <= 1; i++) {
+    for (let j = -1; j <= 1; j++) {
+      if (pos1 == 0) {
+        i++;
+      }
+      if (pos2 == 0) {
+        j++;
+      }
+      if (pos1 == arr.length - 1 && i == 1) {
+        break;
+      }
+      if (pos2 == arr[0].length - 1 && j == 1) {
+        break;
+      }
+      if (arr[pos1 + i][pos2 + j] == "@") {
+        count++;
+      }
+    }
+  }
+  return count - 1;
 }
