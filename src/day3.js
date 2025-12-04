1417cf55e34d690a7621bbc970ade06ef3db8507
eggyolks317 <yokshi25@kas.kh.edu.tw>
2025-12-03 19:49:29 -0800

    completed puzzle 1

--- src/day3.js
+++ src/day3.js
@@ -7,17 +7,38 @@ export function day3() {
     let p2Ans = puzzle2();
     answer1Box.textContent = "answer to puzzle 1: " + p1Ans;
     answer2Box.textContent = "answer to puzzle 2: " + p2Ans;
-    console.log("END CALCULATION");
+    console.log("END CALCULATION : Day 3");
   });
 }
 
-let testCase = ``;
+let testCase = `987654321111111 811111111111119 234234234234278 818181911112111`;
 
 function puzzle1() {
   let input = document.getElementById("input3").value;
   if (input.length == 0) {
     return 0;
   }
+  let banks = input.split(" ");
+  let count = 0;
+  banks.forEach((bank) => {
+    let batteries = bank.split("").map((battery) => parseInt(battery));
+    let max1 = 0;
+    let i1, i2;
+    let max2 = 0;
+    batteries.forEach((battery, index) => {
+      if (battery > max1 && index != batteries.length - 1) {
+        max1 = battery;
+        i1 = index;
+        max2 = batteries[index + 1];
+        i2 = index + 1;
+      } else if (battery > max2 && index > i2) {
+        max2 = battery;
+      }
+    });
+    count += max1 * 10 + max2;
+  });
+  console.log("puzzle1 answer: " + count);
+  return count;
 }
 
 function puzzle2() {
