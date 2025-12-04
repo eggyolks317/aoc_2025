c45fc78a2da028c11dcaf3b3765fd7e45d2fa8d4
eggyolks317 <yokshi25@kas.kh.edu.tw>
2025-12-03 19:20:35 -0800

    init day3.js

---
+++ src/day3.js
@@ -0,0 +1,28 @@
+export function day3() {
+  let submitBtn = document.getElementById("day3Submit");
+  let answer1Box = document.getElementById("answer1Day3");
+  let answer2Box = document.getElementById("answer2Day3");
+  submitBtn.addEventListener("click", () => {
+    let p1Ans = puzzle1();
+    let p2Ans = puzzle2();
+    answer1Box.textContent = "answer to puzzle 1: " + p1Ans;
+    answer2Box.textContent = "answer to puzzle 2: " + p2Ans;
+    console.log("END CALCULATION");
+  });
+}
+
+let testCase = ``;
+
+function puzzle1() {
+  let input = document.getElementById("input3").value;
+  if (input.length == 0) {
+    return 0;
+  }
+}
+
+function puzzle2() {
+  let input = document.getElementById("input3").value;
+  if (input.length == 0) {
+    return 0;
+  }
+}
