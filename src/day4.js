39bcdc53320a2cc4b4d18058895cc1198fa29490
eggyolks317 <yokshi25@kas.kh.edu.tw>
2025-12-03 22:43:29 -0800

    init day4

---
+++ src/day4.js
@@ -0,0 +1,29 @@
+export function day4() {
+  let submitBtn = document.getElementById("day4Submit");
+  let answer1Box = document.getElementById("answer1Day4");
+  let answer2Box = document.getElementById("answer2Day4");
+  submitBtn.addEventListener("click", () => {
+    let p1Ans = puzzle1();
+    let p2Ans = puzzle2();
+    answer1Box.textContent = "answer to puzzle 1: " + p1Ans;
+    answer2Box.textContent = "answer to puzzle 2: " + p2Ans;
+    console.log("END CALCULATION : Day 4");
+    console.log("");
+  });
+}
+
+let testCase = ``;
+
+function puzzle1() {
+  let input = document.getElementById("input4").value;
+  if (input.length == 0) {
+    return 0;
+  }
+}
+
+function puzzle2() {
+  let input = document.getElementById("input4").value;
+  if (input.length == 0) {
+    return 0;
+  }
+}
