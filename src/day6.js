7aedf0f6cea9fa312efb8dbfd0a55bbf50447a69
eggyolks317 <yokshi25@kas.kh.edu.tw>
2025-12-07 14:29:23 -0800

    day 6 puzzle 1 test case worked

--- src/day6.js
+++ src/day6.js
@@ -12,18 +12,63 @@ export function day6() {
   });
 }
 
-let testCase = ``;
+let testCase = `123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  `;
 
 function puzzle1() {
   let input = document.getElementById("input6").value;
+  input = testCase;
   if (input.length == 0) {
     return 0;
   }
+
+  //initializing inputs
+  let lines = input.split("\n");
+  lines = lines.map((line) => {
+    line = line.split(" ");
+    line = removeEmpty(line);
+    return line;
+  });
+
+  for (let i = 0; i < lines.length - 1; i++) {
+    lines[i] = lines[i].map(Number);
+  }
+
+  let operations = lines[lines.length - 1];
+  let answer = new Array(operations.length);
+  answer.fill(0);
+  console.log(operations);
+  for (let i = 0; i < lines.length - 1; i++) {
+    for (let j = 0; j < lines[i].length; j++) {
+      if (answer[j] == 0) {
+        answer[j] = lines[i][j];
+      } else {
+        answer[j] = operate(answer[j], lines[i][j], operations[j]);
+      }
+    }
+  }
+  return answer.reduce((sum, current) => sum + current);
 }
 
 function puzzle2() {
   let input = document.getElementById("input6").value;
   if (input.length == 0) {
     return 0;
+  }
+}
+function removeEmpty(arr) {
+  return arr.filter((item) => item != "");
+}
+function operate(x, y, action) {
+  if (action == "+") {
+    return x + y;
+  } else if (action == "-") {
+    return x - y;
+  } else if (action == "*") {
+    return x * y;
+  } else if (action == "/") {
+    return x / y;
   }
 }
