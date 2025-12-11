8c8980b6fe4327718b9819941453ecbe76f7da81
eggyolks317 <yokshi25@kas.kh.edu.tw>
2025-12-10 16:52:52 -0800

    day 7 puzzle 1 test case worked

--- src/day7.js
+++ src/day7.js
@@ -12,13 +12,53 @@ export function day7() {
   });
 }
 
-let testCase = ``;
+let testCase = `.......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............`;
 
 function puzzle1() {
   let input = document.getElementById("input7").value;
+  input = testCase;
   if (input.length == 0) {
     return 0;
   }
+  //input is a 2d array
+  let lines = input.split("\n");
+  let beams = new Array(lines.length);
+  beams.fill(0);
+  lines = lines.map((line) => line.split(""));
+  let count = 0;
+  lines.forEach((line) => {
+    for (let i = 0; i < line.length; i++) {
+      if (line[i] == "S") {
+        beams[i] = 1;
+      } else if (line[i] == "^" && beams[i] == 1) {
+        count++;
+        if (i != 0) {
+          beams[i - 1] = 1;
+        }
+        if (i != line.input - 1) {
+          beams[i + 1] = 1;
+        }
+        beams[i] = 0;
+      }
+    }
+  });
+  console.log(count);
+  return count;
 }
 
 function puzzle2() {
