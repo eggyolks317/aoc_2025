97f4811ad880b82f187e10444280d9b939b160bd
eggyolks317 <yokshi25@kas.kh.edu.tw>
2025-12-11 21:56:28 -0800

    init day 8 elements

---
+++ src/day8.js
@@ -0,0 +1,29 @@
+export function day8() {
+  let submitBtn = document.getElementById("day8Submit");
+  let answer1Box = document.getElementById("answer1Day8");
+  let answer2Box = document.getElementById("answer2Day8");
+  submitBtn.addEventListener("click", () => {
+    let p1Ans = puzzle1();
+    let p2Ans = puzzle2();
+    answer1Box.textContent = "answer to puzzle 1: " + p1Ans;
+    answer2Box.textContent = "answer to puzzle 2: " + p2Ans;
+    console.log("END CALCULATION : Day 8");
+    console.log("");
+  });
+}
+
+let testCase = ``;
+
+function puzzle1() {
+  let input = document.getElementById("input8").value;
+  if (input.length == 0) {
+    return 0;
+  }
+}
+
+function puzzle2() {
+  let input = document.getElementById("input8").value;
+  if (input.length == 0) {
+    return 0;
+  }
+}
