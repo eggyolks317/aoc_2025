380a6130cc99b43e8b526c59ca7c66146a64400d
eggyolks317 <yokshi25@kas.kh.edu.tw>
2025-12-11 21:50:28 -0800

    day 7 part 2 test case worked

--- src/day7.js
+++ src/day7.js
@@ -1,4 +1,5 @@
 export function day7() {
+  puzzle2();
   let submitBtn = document.getElementById("day7Submit");
   let answer1Box = document.getElementById("answer1Day7");
   let answer2Box = document.getElementById("answer2Day7");
@@ -31,7 +32,6 @@ let testCase = `.......S.......
 
 function puzzle1() {
   let input = document.getElementById("input7").value;
-  input = testCase;
   if (input.length == 0) {
     return 0;
   }
@@ -63,7 +63,36 @@ function puzzle1() {
 
 function puzzle2() {
   let input = document.getElementById("input7").value;
+  input = testCase;
   if (input.length == 0) {
     return 0;
   }
+  //input is a 2d array
+  let lines = input.split("\n");
+  let beams = new Array(lines.length);
+  beams.fill(0);
+  let status = new Array(lines.length);
+  status.fill(0);
+  lines = lines.map((line) => line.split(""));
+  for (let j = 0; j < lines.length; j++) {
+    let line = lines[j];
+    for (let i = 0; i < line.length; i++) {
+      if (line[i] == "S") {
+        status[i] = 1;
+        beams[i] = 1;
+      } else if (line[i] == "^" && beams[i] == 1) {
+        if (i != 0) {
+          beams[i - 1] = 1;
+          status[i - 1] += status[i];
+        }
+        if (i != line.input - 1) {
+          beams[i + 1] = 1;
+          status[i + 1] += status[i];
+        }
+        beams[i] = 0;
+        status[i] = 0;
+      }
+    }
+  }
+  return status.reduce((sum, current) => (sum += current), 0);
 }
